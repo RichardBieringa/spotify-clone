5f3cab0528686824858359b2a918f4c54ba92ca2
Richard Bieringa <richardbieringa@gmail.com>
2022-01-26 14:50:42 +0100

    Updated next config hot reloading (WSL2)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  webpackDevMiddleware: (config) => {
+    // Fixes hot reloading in WSL2
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
+module.exports = nextConfig;
