76093201210ed3a02cb2bbeae95a7da6f6f77854
Richard Bieringa <richardbieringa@gmail.com>
2022-02-10 13:42:13 +0100

    Updated Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,10 @@ import {
   HeartIcon,
   LogoutIcon,
 } from "@heroicons/react/outline";
+import { useState } from "react";
 import { signOut } from "next-auth/react";
+import IconWrapper from "./common/IconWrapper";
+import { BsSpotify } from "react-icons/bs";
 
 interface ButtonProps {
   type: string;
@@ -22,53 +25,81 @@ function SidebarButton({ type, size }: ButtonProps) {
   );
 }
 
-function Sidebar() {
+interface SidebarProps {
+  playlists: SpotifyApi.PlaylistObjectSimplified[];
+}
+
+function Sidebar({playlists}: SidebarProps) {
+
+  const [playlist, setPlaylist] = useState<SpotifyApi.PlaylistObjectSimplified | null>(null)
+
   return (
-    <div className="flex flex-col space-y-2 bg-gray-300">
-      <button
-        className="flex items-center space-x-2 p-4 border-gray-900 text-gray-500 hover:text-white"
-        onClick={() => {
-          signOut();
-        }}
-      >
-        <LogoutIcon className="w-6 h-6" />
-        <span>Sign out</span>
-      </button>
+    <div className="flex flex-col space-y-2 mx-4">
 
-      <button className="flex items-center space-x-2 p-4 border-gray-900 text-gray-500 hover:text-white">
-        <HomeIcon className="w-6 h-6" />
-        <span>Home</span>
-      </button>
+      {/* Logo */}
+      <div className="my-4 flex items-center text-white cursor-pointer hover:text-white">
+        <BsSpotify className="w-10 h-10" />
+        <span className="mx-2 text-xl font-bold">Spotify</span>
+      </div>
 
-      <button className="flex items-center space-x-2 p-4 border-gray-900 text-gray-500 hover:text-white">
-        <SearchIcon className="w-6 h-6" />
-        <span>Seach</span>
-      </button>
+      {/* Buttons */}
+      <div className="flex flex-col">
+        <button
+          className="flex items-center border-gray-900 text-gray-400 hover:text-white"
+          onClick={() => {
+            signOut();
+          }}
+        >
+          <LogoutIcon className="w-6 h-6" />
+          <span>Sign out</span>
+        </button>
 
-      <button className="flex items-center space-x-2 p-4 border-gray-900 text-gray-500 hover:text-white">
-        <LibraryIcon className="w-6 h-6" />
-        <span>Your Library</span>
-      </button>
+        <button className="flex my-2 items-center border-gray-900 font-semibold text-gray-400 hover:text-white transition-colors">
+          <HomeIcon className="w-6 h-8" />
+          <span className="mx-2">Home</span>
+        </button>
+
+        <button className="flex my-2 items-center border-gray-900 font-semibold text-gray-400 hover:text-white transition-colors">
+          <SearchIcon className="w-6 h-6" />
+          <span className="mx-2">Seach</span>
+        </button>
+
+        <button className="flex my-2 items-center border-gray-900 text-gray-400 font-semibold hover:text-white transition-colors">
+          <LibraryIcon className="w-6 h-6" />
+          <span className="mx-2">Your Library</span>
+        </button>
+
+        <br className="my-4"/>
+
+        <button className="flex my-2 items-center border-gray-900 text-gray-400 font-semibold hover:text-white transition-colors">
+          <PlusIcon className="w-6 h-6" />
+          <span className="mx-2">Create Playlist</span>
+        </button>
+
+        <button className="flex my-2 items-center border-gray-900 text-gray-400 font-semibold hover:text-white transition-colors">
+          <HeartIcon className="w-6 h-6" />
+          <span className="mx-2">Liked Songs</span>
+        </button>
+      </div>
+
+
 
       <hr className="border-gray-500" />
 
-      <button className="flex items-center space-x-2 p-4 border-gray-900 text-gray-500 hover:text-white">
-        <PlusIcon className="w-6 h-6" />
-        <span>Create Playlist</span>
-      </button>
-
-      <button className="flex items-center space-x-2 p-4 border-gray-900 text-gray-500 hover:text-white">
-        <HeartIcon className="w-6 h-6" />
-        <span>Liked Songs</span>
-      </button>
-
       {/* Playlist */}
-      <p className="mx-auto cursor-pointer hover:text-white">Playlist x...</p>
-      <p className="mx-auto cursor-pointer hover:text-white">Playlist x...</p>
-      <p className="mx-auto cursor-pointer hover:text-white">Playlist x...</p>
-      <p className="mx-auto cursor-pointer hover:text-white">Playlist x...</p>
-      <p className="mx-auto cursor-pointer hover:text-white">Playlist x...</p>
-      <p className="mx-auto cursor-pointer hover:text-white">Playlist x...</p>
+      <ul className="overflow-y-scroll w-full">
+      {
+          playlists.map((playlist) => {
+            return (
+              <li key={playlist.id}
+                className="py-1 mx-auto overflow-x-hidden whitespace-nowrap text-ellipsis text-gray-400 cursor-pointer hover:text-white"
+                onClick={() => setPlaylist(playlist)}>
+                {playlist.name}
+              </li>
+            )
+          })
+      }
+      </ul>
     </div>
   );
 }
